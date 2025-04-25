7428f6a2d34ddc625355fb81dcfb04a1360a74a0
Khai <khaiaw@stanford.edu>
2025-04-25 10:05:48 -0700

    Improve detection code and write script for visualizing them. TODO: Port changes from vis script to the script that creates csv of detections, to ensure they are ~consistent

--- create_csv_from_pkl.py
+++ create_csv_from_pkl.py
@@ -9,19 +9,92 @@ from tqdm import tqdm
 import ray
 from datetime import timedelta
 
+# TODO: I made changes to the detection algorithm in `vis_pkl_predictions.py`. 
+# I need to port relevant changes over to make them ~consistent.
+
 debug = True
 out_vis_dir = '/ccn2/dataset/babyview/outputs_20250312/pose/4M_frames_old'
 output_csv_path = '/ccn2/dataset/babyview/outputs_20250312/pose/4M_frames_old/test_4M_with_NA_and_bbox.csv'
 num_processes = 16
 threshold_used_for_presence_of_body_part = 0.5
 threshold_used_to_calc_bounding_box = 0.2
+num_keypoints_to_average = 3
 
+# https://github.com/jin-s13/COCO-WholeBody
 body_parts = {
-    'body': range(0,17),
-    'face': range(17,85),
-    'hands': range(85,127),
-    'feet': range(127,133),
+    'body': list(range(6, 18)),
+    'left_foot': list(range(18, 21)),
+    'right_foot': list(range(21, 24)),
+    'face': list(range(1, 6)) + list(range(24, 91)),
+    'left_hand': list(range(92, 113)),
+    'right_hand': list(range(113, 134)),
 }
+
+# convert from 1-based to 0-based indexing
+for part, indices in body_parts.items():
+    body_parts[part] = [i - 1 for i in indices]
+
+def compute_iou_of_bboxes(bbox1, bbox2):
+    # Calculate the intersection area
+    x1 = max(bbox1[0], bbox2[0])
+    y1 = max(bbox1[1], bbox2[1])
+    x2 = min(bbox1[2], bbox2[2])
+    y2 = min(bbox1[3], bbox2[3])
+    
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+    
+    # Calculate the union area
+    bbox1_area = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+    bbox2_area = (bbox2[2] - bbox2[0]) * (bbox2[3] - bbox2[1])
+    
+    union_area = bbox1_area + bbox2_area - intersection_area
+    
+    # Calculate IoU
+    iou_value = intersection_area / union_area if union_area > 0 else 0
+    return iou_value
+
+def check_if_first_bbox_inside_second(bbox1, bbox2):
+    # Check if bbox1 is inside bbox2
+    x_length, y_length = bbox1[2] - bbox1[0], bbox1[3] - bbox1[1]
+    if (bbox1[0] + 0.2 * x_length >= bbox2[0] and
+        bbox1[1] + 0.2 * y_length >= bbox2[1] and
+        bbox1[2] - 0.2 * x_length <= bbox2[2] and
+        bbox1[3] - 0.2 * y_length <= bbox2[3]):
+        return True
+    return False
+
+def compute_percent_of_bbox_inside_other_bbox(bbox1, bbox2):
+    # Calculate the intersection area
+    x1 = max(bbox1[0], bbox2[0])
+    y1 = max(bbox1[1], bbox2[1])
+    x2 = min(bbox1[2], bbox2[2])
+    y2 = min(bbox1[3], bbox2[3])
+    
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+    
+    # Calculate the area of bbox1
+    bbox1_area = (bbox1[2] - bbox1[0]) * (bbox1[3] - bbox1[1])
+    
+    # Calculate the percentage of bbox1 inside bbox2
+    percent_inside = intersection_area / bbox1_area if bbox1_area > 0 else 0
+    return percent_inside
+
+def get_valid_person_bbox_bool_list(person_bbox_list):
+    # if there is a bounding box within another, we should discard the one inside. 
+    # however, in the scenario where there are two boxes that are almost exactly the same spot and size, 
+    # we should keep one of them (!)
+    valid_person_bbox_bool_list = [True for _ in range(len(person_bbox_list))]
+    for i, person_bbox in enumerate(person_bbox_list):
+        
+        # check if the bounding box is within another bounding box
+        for j, other_person_bbox in enumerate(person_bbox_list):
+            if i == j or other_person_bbox is None:
+                continue
+            
+            if check_if_first_bbox_inside_second(person_bbox, other_person_bbox):
+                valid_person_bbox_bool_list[i] = False
+                break
+    return valid_person_bbox_bool_list
 
 def get_average_scores_per_body_part(keypoint_scores):
     # keypoint_scores = get_keypoint_scores(model, img_path)
@@ -30,9 +103,13 @@ def get_average_scores_per_body_part(keypoint_scores):
     
     body_part_scores = {}
     for body_part, indices in body_parts.items():
+        if body_part == 'feet':
+            num_keypoints = 1
+        else:
+            num_keypoints = num_keypoints_to_average
         scores = keypoint_scores[list(indices)]
         # get the top N scores and average them
-        scores = np.sort(scores)[::-1][:2]
+        scores = np.sort(scores)[::-1][:num_keypoints]
         scores = np.mean(scores)
         body_part_scores[body_part] = min(1.0, scores)
         body_part_scores[body_part] = round(float(body_part_scores[body_part]), 2)
@@ -67,19 +144,32 @@ def get_bounding_boxes_per_body_part(keypoints, keypoint_scores):
         body_part_bbox_sizes[body_part] = body_part_bbox_size
     return body_part_bounding_boxes, body_part_bbox_sizes
 
+def detect_if_body_part_in_image(keypoint_scores):
+    average_scores_per_body_part = get_average_scores_per_body_part(keypoint_scores)
+
+    body_in_image = int(average_scores_per_body_part['body'] >= threshold_used_for_presence_of_body_part)
+    face_in_image = int(average_scores_per_body_part['face'] >= threshold_used_for_presence_of_body_part)
+    left_hand_in_image = int(average_scores_per_body_part['left_hand'] >= threshold_used_for_presence_of_body_part)
+    right_hand_in_image = int(average_scores_per_body_part['right_hand'] >= threshold_used_for_presence_of_body_part)
+    left_foot_in_image = int(average_scores_per_body_part['left_foot'] >= threshold_used_for_presence_of_body_part)
+    right_foot_in_image = int(average_scores_per_body_part['right_foot'] >= threshold_used_for_presence_of_body_part)
+    
+    return body_in_image, face_in_image, left_hand_in_image, right_hand_in_image, left_foot_in_image, right_foot_in_image
+    
 # create a pandas dataframe to store the results
 def create_dataframe(vis_pkl_paths):
-    # TODO: Add bounding box for person, then faces and hands
-    # TODO: Add size of boxes
-    # TODO: Check the resolution of the image frame, resolution of model output, and that x,y is correct
     df_out = pd.DataFrame(columns=['superseded_gcp_name_feb25', 'time_in_extended_iso', 
                                    'person_detected', 
                                    'person_bbox_xyxy', 
                                    'person_bbox_conf', 'person_bbox_size',
                                    'body_score', 'face_score', 'hands_score', 'feet_score', 
-                                   'body_in_image', 'face_in_image', 'hands_in_image', 'feet_in_image', 
-                                   'body_bounding_box_xyxy', 'face_bounding_box_xyxy', 'hands_bounding_box_xyxy', 'feet_bounding_box_xyxy',
-                                   'body_bounding_box_size', 'face_bounding_box_size', 'hands_bounding_box_size', 'feet_bounding_box_size',
+                                   'body_in_image', 'face_in_image', 'left_hand_in_image', 'right_hand_in_image', 'left_foot_in_image', 'right_foot_in_image',
+                                   'body_bounding_box_xyxy', 'face_bounding_box_xyxy', 
+                                   'left_hand_bounding_box_xyxy', 'right_hand_bounding_box_xyxy', 
+                                   'left_foot_bounding_box_xyxy', 'right_foot_bounding_box_xyxy',
+                                   'body_bounding_box_size', 'face_bounding_box_size', 
+                                   'left_hand_bounding_box_size', 'right_hand_bounding_box_size',
+                                   'left_foot_bounding_box_size', 'right_foot_bounding_box_size',
                                    ], index=None)
 
     for i, pkl_path in enumerate(tqdm(vis_pkl_paths, desc="Processing PKL files")):
@@ -106,10 +196,7 @@ def create_dataframe(vis_pkl_paths):
                 keypoint_scores = per_person_pose_dict['keypoint_scores']
                 average_scores_per_body_part = get_average_scores_per_body_part(keypoint_scores)
 
-                body_in_image = int(average_scores_per_body_part['body'] >= threshold_used_for_presence_of_body_part)
-                face_in_image = int(average_scores_per_body_part['face'] >= threshold_used_for_presence_of_body_part)
-                hand_in_image = int(average_scores_per_body_part['hands'] >= threshold_used_for_presence_of_body_part)
-                feet_in_image = int(average_scores_per_body_part['feet'] >= threshold_used_for_presence_of_body_part)
+                body_in_image, face_in_image, left_hand_in_image, right_hand_in_image, left_foot_in_image, right_foot_in_image = detect_if_body_part_in_image(keypoint_scores)
 
                 keypoints = per_person_pose_dict['keypoints']
                 bounding_boxes_per_body_part, body_part_bbox_sizes = get_bounding_boxes_per_body_part(keypoints, keypoint_scores)
@@ -132,15 +219,19 @@ def create_dataframe(vis_pkl_paths):
                     'feet_score': average_scores_per_body_part['feet'],
                     'body_in_image': body_in_image,
                     'face_in_image': face_in_image,
-                    'hands_in_image': hand_in_image,
-                    'feet_in_image': feet_in_image,
+                    'left_hand_in_image': left_hand_in_image,
+                    'right_hand_in_image': right_hand_in_image,
+                    'left_foot_in_image': left_foot_in_image,
+                    'right_foot_in_image': right_foot_in_image,
                     'body_bounding_box_xyxy': bounding_boxes_per_body_part['body'],
                     'face_bounding_box_xyxy': bounding_boxes_per_body_part['face'],
-                    'hands_bounding_box_xyxy': bounding_boxes_per_body_part['hands'],
+                    'left_hand_bounding_box_xyxy': bounding_boxes_per_body_part['left_hand'],
+                    'right_hand_bounding_box_xyxy': bounding_boxes_per_body_part['right_hand'],
                     'feet_bounding_box_xyxy': bounding_boxes_per_body_part['feet'],
                     'body_bounding_box_size': body_part_bbox_sizes['body'],
                     'face_bounding_box_size': body_part_bbox_sizes['face'],
-                    'hands_bounding_box_size': body_part_bbox_sizes['hands'],
+                    'left_hand_bounding_box_size': body_part_bbox_sizes['left_hand'],
+                    'right_hand_bounding_box_size': body_part_bbox_sizes['right_hand'],
                     'feet_bounding_box_size': body_part_bbox_sizes['feet'],
                 }
                 
@@ -150,30 +241,32 @@ def create_dataframe(vis_pkl_paths):
 def create_dataframe_remote(vis_pkl_paths):
     return create_dataframe(vis_pkl_paths)
 
-vis_pkl_paths = glob.glob(os.path.join(out_vis_dir, '**/*.pkl'), recursive=True)
-random.shuffle(vis_pkl_paths)
-print('Number of PKL files:', len(vis_pkl_paths))
+if __name__ == '__main__':
+    vis_pkl_paths = glob.glob(os.path.join(out_vis_dir, '**/*.pkl'), recursive=True)
+    random.shuffle(vis_pkl_paths)
+    print('Number of PKL files:', len(vis_pkl_paths))
 
-if debug:
-    vis_pkl_paths = vis_pkl_paths[:100]
-    # debug_df = create_dataframe(vis_pkl_paths)
-    # debug_df.to_csv(output_csv_path, index=False)
-    # exit()
+    if debug:
+        vis_pkl_paths = vis_pkl_paths[:100]
+        # debug_df = create_dataframe(vis_pkl_paths)
+        # debug_df.to_csv(output_csv_path, index=False)
+        # exit()
 
-# Split the PKL paths into chunks for parallel processing
-ray.init(num_cpus=num_processes)
-num_chunks = num_processes
-chunks = np.array_split(vis_pkl_paths, num_chunks)
+    # Split the PKL paths into chunks for parallel processing
+    # Initialize Ray
+    ray.init(num_cpus=num_processes)
+    num_chunks = num_processes
+    chunks = np.array_split(vis_pkl_paths, num_chunks)
 
-# Process each chunk in parallel
-dataframes = ray.get([create_dataframe_remote.remote(chunk) for chunk in chunks])
+    # Process each chunk in parallel
+    dataframes = ray.get([create_dataframe_remote.remote(chunk) for chunk in chunks])
 
 
-# Combine all dataframes into a single dataframe
-final_dataframe = pd.concat(dataframes, ignore_index=True)
+    # Combine all dataframes into a single dataframe
+    final_dataframe = pd.concat(dataframes, ignore_index=True)
 
-# Save the combined dataframe to a CSV file
-final_dataframe.to_csv(output_csv_path, index=False)
+    # Save the combined dataframe to a CSV file
+    final_dataframe.to_csv(output_csv_path, index=False)
 
-# Print some statistics
-print(final_dataframe.round(2).describe())
+    # Print some statistics
+    print(final_dataframe.round(2).describe())
